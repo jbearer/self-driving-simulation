562bd0345b422f21a0840fa6ab4ac68d33d5ea56
Jeb Bearer <jbearer@hmc.edu>
2016-10-22 04:13:26 -0700

    Add a simple, single-motor test

---
+++ src/hardware/test/test_motor_simple.cpp
@@ -0,0 +1,35 @@
+#include <memory>
+#include <unistd.h>
+
+#include "hardware/motor.h"
+#include "logging/logging.h"
+#include "objects/objects.h"
+
+using namespace std;
+using namespace hardware;
+
+static logging::logger diag("test/motor.simple");
+
+int main()
+{
+    diag.info("Initializing motor");
+    auto factory = objects::get<motor_factory>();
+    auto motor = factory->create(0);
+
+    diag.info("Sleeping for 2 seconds with motors idle.");
+    usleep(2e6);
+
+    diag.info("Accelerating motor 0.");
+    motor->set_acceleration(0.2);
+    usleep(2e6);
+    diag.info("Decelerating motor 0.");
+    motor->change_acceleration(-0.2);
+    usleep(2e6);
+
+    diag.info("Sleeping for 2 seconds with motors once again idle.");
+    usleep(2e6);
+
+    diag.info("Stopping.");
+
+    return 0;
+}
