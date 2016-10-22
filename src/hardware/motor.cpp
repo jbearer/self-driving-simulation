7528e07810b50ed3eb6716b9e37fb6b760cf1c77
Jeb Bearer <jbearer@hmc.edu>
2016-10-22 07:19:32 -0700

    ???

--- src/hardware/motor.cpp
+++ src/hardware/motor.cpp
@@ -148,7 +148,7 @@ private:
             {
                 lock_guard<mutex> lock(data_lock);
 
-                double v_0 = 2 * (velocity_offset / RADIUS) * 60 / (2 * PI);
+                double v_0 =  ((velocity_offset / RADIUS) * 60 / (2 * PI)) / 2;
                 diag.info("v_0 = {}", v_0);
                 double a   = (accel / RADIUS) * 60 / (2 * PI); // Linear to angular
                 curr = next;
