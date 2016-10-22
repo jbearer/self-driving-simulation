f63567666283acc703fb237afecff9ca20319578
Jeb Bearer <jbearer@hmc.edu>
2016-10-22 05:25:33 -0700

    Change sensor interface

--- include/hardware/sensor.h
+++ include/hardware/sensor.h
@@ -10,19 +10,7 @@ namespace hardware
 {
     struct sensor
     {
-        /**
-         * @brief      Register a callback to execute when the sensor passes a certain threshold.
-         *
-         * @param[in]  name       A unique name identifying the callback.
-         */
-        virtual void add_callback(std::string name, double threshold, std::function<void(void)>) = 0;
-
-        /**
-         * @brief      Removes a callback.
-         *
-         * @param[in]  name  The name passed to add_callback.
-         */
-        virtual void remove_callback(std::string name) = 0;
+        virtual bool is_triggered() const = 0;
 
         virtual ~sensor() {};
     };
--- src/hardware/sensor.cpp
+++ src/hardware/sensor.cpp
@@ -1,20 +1,67 @@
+#include <algorithm>
+#include <atomic>
 #include <functional>
 #include <memory>
 #include <string>
+#include <thread>
 
 #include "logging/logging.h"
+#include "hardware/raspi.h"
 #include "hardware/sensor.h"
+#include "objects/objects.h"
 
 using namespace std;
 using namespace hardware;
 
 static logging::logger diag("hardware/sensor");
 
+// Mapping from track_id_s to pins
+static unordered_map<int, int> pins = {
+    {0, 0},
+    {1, 1},
+    {2, 2},
+    {3, 3},
+    {4, 4},
+    {5, 5}
+};
+
+struct sensor_impl
+    : sensor
+{
+    explicit sensor_impl(int pin_)
+        : pi( objects::get<raspi>() )
+        , pin(pin_)
+    {
+        pi->pin_mode(pin, raspi::INPUT);
+    }
+
+    bool is_triggered() const
+    {
+        return pi->digital_read(pin) == raspi::HIGH;
+    }
+
+private:
+    shared_ptr<raspi>   pi;
+    int                 pin;
+};
+
 struct mock_sensor
     : sensor
 {
-    void add_callback(string, double, function<void()>) {}
-    void remove_callback(string) {}
+    bool is_triggered() const
+    {
+        return false;
+    }
+};
+
+struct sensor_factory_impl
+    : sensor_factory
+{
+    unique_ptr<sensor> create(int track_id)
+    {
+        int pin = pins[track_id];
+        return unique_ptr<sensor>( static_cast<sensor *>( new sensor_impl(pin) ) );
+    }
 };
 
 struct mock_sensor_factory
@@ -26,4 +73,5 @@ struct mock_sensor_factory
     }
 };
 
-register_object(sensor_factory, mock_sensor_factory);
+register_object(sensor_factory, sensor_factory_impl);
+register_mock_object(sensor_factory, mock_sensor_factory);
