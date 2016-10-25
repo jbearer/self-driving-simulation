855e2ed324e9744c2bc6468b8451e396ed3bf324
Jeb Bearer <jbearer@hmc.edu>
2016-10-24 17:37:58 -0700

    Create logger_impl base class with extensible logging mechanism.
    Uses CRTP to allow child classes to capture incoming log messages,
    modify them, and then pass them up the class hierarchy to be logged.

--- include/logging/logging.h
+++ include/logging/logging.h
@@ -21,46 +21,43 @@ namespace logging
         });
     }
 
-    /**
-     * @brief      Logging mechanism. Every file should have one logger, declared like so:
-     *                  static logger diag("short, descriptive name");
-     *             Used:
-     *                  diag->info("Formatted message with {} param", 1);
-     */
-    class logger
+    template<typename impl_t>
+    struct logger_impl
     {
-    public:
-
-        logger(std::string const & name)
+        logger_impl(std::string const & name)
             : inner( spdlog::get(name) )
         {
             inner = inner ? inner : spdlog::basic_logger_mt(name, LOG_FILE, false);
             inner->flush_on( inner->level() );
         }
 
+#define impl_log(lvl) static_cast<impl_t *>(this)->log(log_level::lvl, fmt, args...)
+
         template <typename... args_t> void trace(char const * fmt, const args_t&... args) {
-            inner->trace(fmt, args...);
+            impl_log(trace);
         }
 
         template <typename... args_t> void debug(char const * fmt, const args_t&... args) {
-            inner->debug(fmt, args...);
+            impl_log(debug);
         }
 
         template <typename... args_t> void info(char const * fmt, const args_t&... args) {
-            inner->info(fmt, args...);
+            impl_log(info);
         }
 
         template <typename... args_t> void warn(char const * fmt, const args_t&... args) {
-            inner->warn(fmt, args...);
+            impl_log(warn);
         }
 
         template <typename... args_t> void error(char const * fmt, const args_t&... args) {
-            inner->error(fmt, args...);
+            impl_log(err);
         }
 
         template <typename... args_t> void critical(char const * fmt, const args_t&... args) {
-            inner->critical(fmt, args...);
+            impl_log(critical);
         }
+
+#undef impl_log
 
         /**
          * @brief      Used when a fatal condition is encountered.
@@ -71,7 +68,7 @@ namespace logging
         template <typename... args_t> void fail(char const * fmt, const args_t&... args) {
             std::string msg("FATAL: ");
             msg.append(fmt);
-            inner->critical(msg.c_str(), args...);
+            critical(msg.c_str(), args...);
             std::abort();
         }
 
@@ -81,7 +78,55 @@ namespace logging
             inner->flush_on(level);
         }
 
+        std::string name() const
+        {
+            return inner->name();
+        }
+
+    protected:
+        template <typename... args_t>
+        void log(log_level level, const char* fmt, const args_t&... args)
+        {
+            inner->log(level, fmt, args...);
+        }
+
     private:
         std::shared_ptr<spdlog::logger> inner;
+    };
+
+    /**
+     * @brief      Logging mechanism. Every file should have one logger, declared like so:
+     *                  static logger diag("short, descriptive name");
+     *             Used:
+     *                  diag->info("Formatted message with {} param", 1);
+     */
+    struct logger
+        : logger_impl<logger>
+    {
+        logger(std::string const & name)
+            : logger_impl(name)
+        {}
+    };
+
+    struct prefix_logger
+        : logger_impl<prefix_logger>
+    {
+        prefix_logger(std::string const & name, std::string const & prefix_)
+            : logger_impl(name)
+            , prefix(prefix_)
+        {}
+
+        template<typename... args_t>
+        void log(log_level level, const char * fmt, const args_t&... args)
+        {
+            std::string new_fmt = prefix;
+            new_fmt.append(": ");
+            new_fmt.append(fmt);
+            logger_impl::log(level, new_fmt.c_str(), args...);
+        }
+
+    private:
+
+        std::string prefix;
     };
 }
